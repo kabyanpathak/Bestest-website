dc97e7412484143415c3c4da9a4eb2a86abba5ed
kabyanpathak <kabyan388@gmail.com>
2025-05-23 03:31:42 -0700

    changes

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,23 @@
-use axum::{Router, routing::get};
+use axum::{Router, response::Html, routing::get};
+use std::fs;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route(
-        "/",
-        get(|| async { "~/Documents/Bestest-website/frontend/mainpage.html" }),
-    );
+    //gets the css file
+    let app = Router::new().route("/", get(main_page()));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:7878")
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+fn main_page() -> Html<String> {
+    let filepath = "../frontend/mainpage.html";
+
+    match fs::read_to_string(filepath) {
+        Ok(html) => Html(html),
+
+        Err(_e) => panic!("you fucked up"),
+    }
 }
